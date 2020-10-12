0fe1ec7c5f674d64ce4618f2cc894d86d7b4131a
Kalesh <kaleshcv2@gmail.com>
2020-10-12 18:27:12 +0530

    10/12

--- Production_Data/Prostar.py
+++ Production_Data/Prostar.py
@@ -14,25 +14,29 @@ engine=create_engine(DATABASE_CONNECTION)
 connection=engine.connect()
 
 #   Reading Excel
-df_sheet1=pd.read_excel('//172.16.10.12/PowerBI Reports/ACTIVE SPORT CLUB/Active Sports Club PBI.xlsx',sheet_name='Sheet1')
+df_sheet1=pd.read_excel('//172.16.10.12/PowerBI Reports/PROTOSTAR/PROSTAR PBI.xlsx',sheet_name='Sheet1')
 
-# First Insertion - Done
-#df_sheet1.to_sql('Active_sport',con=connection,if_exists='append',index=False)
+
+# First Insertion - DONE
+#df_sheet1.to_sql('Prostar',con=connection,if_exists='append',index=False)
 
 #   Reeding from SQL
-df_from_sql_sheet1=pd.read_sql('SELECT * FROM Active_sport',con=connection)
+df_from_sql_sheet1=pd.read_sql('SELECT * FROM Prostar',con=connection)
 
 #   Finding Last index
 l_sheet1=df_from_sql_sheet1.index[-1]
 l_sheet1+=1
 print(l_sheet1)
 
+
 # Data to Database sheet1
 df_new_sheet1=df_sheet1.iloc[l_sheet1:]
 print(df_new_sheet1)
 
+
+#print(df_new_sheet1)
+
 # Writing to SQL
-df_new_sheet1.to_sql('Active_sport',con=connection,if_exists='append',index=False)
-print("DONE !")
-#10/6/2020 - 3.57PM
-#10/7/2020 - 12.24PM
+df_new_sheet1.to_sql('Prostar',con=connection,if_exists='append',index=False)
+
+#10/8/2020 - 8.31PM
